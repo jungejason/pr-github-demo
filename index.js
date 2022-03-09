5daea2cb0c57f930719086e7608bae22af80adbc
Jason Ge <gejun_1978@yahoo.com>
2022-03-09 12:14:23 -0800

    DO NOT MERGE - Design Review for ef-3
    Summary:
    I think these are the places to modify.
    
    Test:
    none

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
+// define a const for the port
 
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(3000)
+app.listen(3000) // <- change the number to be a const
