8ead145594b536cd6f5267a8f626d69ad4fb3cf0
Jason Ge <gejun_1978@yahoo.com>
2022-03-09 12:00:24 -0800

    DO NOT MERGE - design review for ef-3
    Summary:
    Are these the right place to implemement the feature?
    
    Test:
    None

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var app = express()
 
+// define a variable for the port
+
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
+// change the port number here to be a variable
 app.listen(3000)
